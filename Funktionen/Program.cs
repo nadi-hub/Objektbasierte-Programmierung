7052dc7061ae9762edad97861e6a79a345eac8f2
nadi-hub <nadja_reisacher@hotmail.com>
2024-08-19 15:56:51 +0200

    initialized MyMath

--- Funktionen/Program.cs
+++ Funktionen/Program.cs
@@ -8,9 +8,27 @@
             int b = 10;
             int ggT = MyMath.Calc_ggT(a, b);
             int kgV = MyMath.Calc_kgV(a, b);
-            Console.WriteLine(ggT);
-            Console.WriteLine(kgV);
-            
+            ShowResult("ggT", a, b, ggT);
+            ShowResult("kgV", a, b, kgV);
+            int eingabe = ReadInt();
+            Console.WriteLine(eingabe);
+        }
+        static int ReadInt()
+        {
+            int x;
+            do
+            {
+                Console.WriteLine("Gib eine Zahl ein");
+                string s = Console.ReadLine();
+                int.TryParse(s, out x);
+            } while (x <= 0);
+
+            Console.WriteLine("Danke");
+            return x;
+        }
+        static void ShowResult(string operation, int a, int b, int result)
+        {
+            Console.WriteLine(operation + $" von {a} und {b} ist {result}");
         }
     }
 }
